e06147f711c7995013ddb42575504bbeaaad306f
Ridwan <r.arifin123@gmail.com>
2019-09-24 10:29:49 +0700

    update v5.3

--- staticfiles/static-root/admin/js/myscript.f6b609692290.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    'use strict'
-    $(document).ready(function() {
-        $('#id_fasilitas').multiSelect();
-        $("input[name='harga_0']").css({"margin-right": "20px"})
-        $(".field-fasilitas .help").remove()
-    });
-})(django.jQuery);
