e9af3a5b87aa6423efb636a522bb3c171e273a7b
Ridwan <r.arifin123@gmail.com>
2019-09-25 11:29:59 +0700

    update allow host

--- website_umroh/settings.py
+++ website_umroh/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
